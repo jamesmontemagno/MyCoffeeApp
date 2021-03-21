ffc4aed587e88bf9b5fc9d237c11276ccaaed247
James Montemagno <james.montemagno@gmail.com>
2021-03-21 14:32:53 -0700

    Navigation!

--- MyCoffeeApp/MyCoffeeApp/AppShell.xaml.cs
+++ MyCoffeeApp/MyCoffeeApp/AppShell.xaml.cs
@@ -1,10 +1,19 @@
-﻿namespace MyCoffeeApp
+﻿using MyCoffeeApp.Views;
+using Xamarin.Forms;
+
+namespace MyCoffeeApp
 {
     public partial class AppShell : Xamarin.Forms.Shell
     {
         public AppShell()
         {
             InitializeComponent();
+
+            Routing.RegisterRoute(nameof(AddMyCoffeePage),
+                typeof(AddMyCoffeePage));
+
+            Routing.RegisterRoute(nameof(MyCoffeeDetailsPage),
+                typeof(MyCoffeeDetailsPage));
         }
     }
 }
