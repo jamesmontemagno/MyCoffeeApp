5645e23bcfc9dbc4938e403122f3d03b71ed58cf
James Montemagno <james.montemagno@gmail.com>
2021-05-24 13:42:16 -0700

    Dependency Injection Sample!

---
+++ MyCoffeeApp/MyCoffeeApp/Services/ICoffeeService.cs
@@ -0,0 +1,14 @@
+﻿using MyCoffeeApp.Shared.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace MyCoffeeApp.Services
+{
+    public interface ICoffeeService
+    {
+        Task AddCoffee(string name, string roaster);
+        Task<IEnumerable<Coffee>> GetCoffee();
+        Task<Coffee> GetCoffee(int id);
+        Task RemoveCoffee(int id);
+    }
+}
---
+++ MyCoffeeApp/MyCoffeeApp/Services/IToast.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyCoffeeApp.Services
+{
+    public interface IToast
+    {
+        void MakeToast(string message);
+    }
+}
