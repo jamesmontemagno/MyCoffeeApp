5bbc82224778ce57bebbec94a9c04a6638a43c9c
James Montemagno <james.montemagno@gmail.com>
2021-10-10 20:58:24 -0700

    Add tab samples

---
+++ Tab-Sample/App2/App2/TabbedPage1.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace App2
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class TabbedPage1 : TabbedPage
+    {
+        public TabbedPage1()
+        {
+            InitializeComponent();
+
+        }
+
+        private async void Button_Clicked(object sender, EventArgs e)
+        {
+            await Tab2.Navigation.PushAsync(new MainPage());
+        }
+    }
+}
