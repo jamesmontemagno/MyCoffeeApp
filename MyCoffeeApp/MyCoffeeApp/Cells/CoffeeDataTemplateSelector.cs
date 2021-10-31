f607547b5256521af1494cdbdbb0c79383e80d72
James Montemagno <james.montemagno@gmail.com>
2021-10-30 18:12:00 -0700

    Example of reusable cells and DTS

---
+++ MyCoffeeApp/MyCoffeeApp/Cells/CoffeeDataTemplateSelector.cs
@@ -0,0 +1,29 @@
+﻿using MyCoffeeApp.Shared.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace MyCoffeeApp.Cells
+{
+    public class CoffeeDataTemplateSelector : DataTemplateSelector
+    {
+        public CoffeeDataTemplateSelector()
+        {
+
+        }
+        public DataTemplate Normal { get; set; }
+        public DataTemplate Special { get; set; }
+
+        protected override DataTemplate OnSelectTemplate(object item, BindableObject container)
+        {
+            //var coffee = (Coffee)item; 
+
+            //return coffee.Roaster == "Yes Plz" ? Special : Normal;
+
+
+            // enable code above for true data template selectors
+            return Normal;
+        }
+    }
+}
